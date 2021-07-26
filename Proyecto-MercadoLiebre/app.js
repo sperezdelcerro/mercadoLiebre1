2ad94782993486a09d75165f1b430086c00a0007
sperezdelcerro <sperezdelcerro@icloud.com>
2021-07-26 19:50:47 -0300

    procfile

--- Proyecto-MercadoLiebre/app.js
+++ Proyecto-MercadoLiebre/app.js
@@ -6,7 +6,7 @@ const app = express();
 const publicPath = path.resolve(__dirname,"./public");
 app.use (express.static(publicPath) );
 
-app.listen (3000, () => {
+app.listen (process.env.PORT || 3000, function()  {
     console.log("servidor corriendo en el puerto 3000");
 });
 
